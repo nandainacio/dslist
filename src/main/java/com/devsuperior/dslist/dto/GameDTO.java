1026b1911ab22c213efa5260762643b695460600
nandainacio <fp_inacio@hotmail.com>
2025-01-25 17:06:36 -0300

    feat: GET by Id

---
+++ src/main/java/com/devsuperior/dslist/dto/GameDTO.java
@@ -0,0 +1,106 @@
+package com.devsuperior.dslist.dto;
+
+import com.devsuperior.dslist.entities.Game;
+
+public class GameDTO {
+
+    private Long id;
+    private String title;
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    public GameDTO(){
+
+    }
+
+    public GameDTO(Game entity) {
+        //pode ser usado o BeanUtils.copyproperties(entity, this);
+        //para obter o mesmo resultado abaixo.
+        id = entity.getId();
+        title = entity.getTitle();
+        year = entity.getYear();
+        genre = entity.getGenre();
+        platforms = entity.getPlatforms();
+        score = entity.getScore();
+        imgUrl = entity.getImgUrl();
+        shortDescription = entity.getShortDescription();
+        longDescription = entity.getLongDescription();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+}
